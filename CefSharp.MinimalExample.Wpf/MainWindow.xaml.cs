9fb15977f0ac99d61861c1bf8937d4575c33689b
Andy Clarke <andy.clarke@okta.com>
2020-08-31 10:50:07 -0400

    Add project files.

---
+++ CefSharp.MinimalExample.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace CefSharp.MinimalExample.Wpf
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void iwaButtonClick(object sender, RoutedEventArgs e)
+        {
+
+            //IWA_user.Text = "tracy.brown";
+
+
+        }
+    }
+}
